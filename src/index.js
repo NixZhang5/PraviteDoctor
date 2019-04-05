7eeb23abec94e70fbcad5c8414fde7268978da38
莲子心 <351235445@qq.com>
2019-04-06 00:33:15 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+                <ul>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/articles">Articles</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Articles</h1>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class Articles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Articles</h1>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+render((
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/about" component={About} />
+            <Route path="/articles" component={Articles} />
+        </div>
+    </BrowserRouter>
+), document.getElementById('root'))
+
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: path.join(__dirname, './src/index.html')}),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        host: '127.0.0.1',
+        port: '8080',
+        contentBase: './dist',
+        hot: true,
+        open: true,
+    }
+}
